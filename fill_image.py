38c32d0e28eecb527caf39115ded4311ca29ed35
Madelyne Xiao <madelyne.xiao96@gmail.com>
2019-02-10 09:51:08 -0500

    updated fill_image

--- fill_image.py
+++ fill_image.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 ## create a filled image using cv2's dilation/erosion functions ##
 
 def make_fill(input_img, output_img):
-	work_file = os.listdir(input_img)[0]
-	img = cv2.imread(work_file,0)
+	img = cv2.imread(input_img,0)
 	print(img)
 	kernel = np.ones((100,100),np.uint8)
 	closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
