cab242652acc076ea1e4c811250c1c61c92f5ecb
matt.m.casey@gmail.com <matt.m.casey@gmail.com>
2019-02-10 11:27:05 -0500

    added line-draw code

--- draw_folder.py
+++ draw_folder.py
@@ -1,15 +1,14 @@
-# from PIL import Image
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from shapely.geometry.polygon import LinearRing, Polygon
+from sklearn.linear_model import LinearRegression
+import pandas as pd
 import os
 import cv2 as cv
 
 """
 Copy this file to the folder containing the tifs you want to convert
-
 run from command line like this: python draw_folder.py
-
-
 """
 
 tifs = [x for x in os.listdir('./T152_Full') if '.tif' in x]
@@ -18,7 +17,6 @@ def make_binary_image(image_file):
     """
     image_file:
         (str), the location of the tif file
-
     outputs a  binarized image with points of interest highlighted
     """
     print('processing: ' + image_file)
@@ -48,6 +46,51 @@ def make_binary_image(image_file):
 
     cv.imwrite('./output/'+image_file, thresh1)
 
+
+
+def outline_shape(csvfile):
+    """
+    csvfile:
+        (str), the target csv file. csv file should have x and y coordinates and
+        no header
+    """
+
+    df = pd.read_table(csvfile, header=None, delimiter=',')
+    lr = LinearRegression()
+
+    lr.fit(df[0].reshape(-1, 1), df[1].reshape(-1, 1))
+
+    df[2] = [lr.predict(a) for a in df[0]]
+    df[3] = df[1] > df[2]
+
+    df = df.sort_values([3, 0])
+
+    coords = []
+
+    for i, r in df[df[3] == False].iterrows():
+        coords.append([r[0], r[1]])
+
+    for i, r in df[df[3] == True][::-1].iterrows():
+        coords.append([r[0], r[1]])
+
+    ring = LinearRing(coords)
+    x, y = ring.xy
+
+    fig = plt.figure(1, figsize=(15,15), dpi=90)
+    ax = fig.add_subplot(111)
+    ax.plot(x, y)
+
+    xrange = [0, 5000]
+    yrange = [0, 1000]
+    ax.set_xlim(*xrange)
+    ax.set_ylim(*yrange)
+    ax.set_aspect(1)
+    plt.axis('off')
+
+    nmsplt = csvfile.split('.')
+    newname = nmsplt[-2] + '.jpg'
+
+    plt.savefig(newname)
 
 if __name__ == '__main__':
     for t in tifs:
